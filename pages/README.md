2b7b18b6c5c518d047be37687f651ec2821fc643
💩 <69432552+unchidev@users.noreply.github.com>
2021-02-16 21:41:49 +0900

    基本機能実装

---
+++ pages/README.md
@@ -0,0 +1,6 @@
+# 瞑想プレイヤー
+
+瞑想に最適なBGMを指定した時間だけ再生することができるプレイヤーです。
+リラックス効果があるので睡眠用BGMや作業用BGMに使うことも可能です。
+
+[https://meditation.koeda.me/](https://meditation.koeda.me/)
---
+++ pages/index.vue
@@ -0,0 +1,118 @@
+<template lang="pug">
+.container
+  .section
+    .content
+      h1 瞑想プレイヤー
+      p 瞑想に最適なBGMを指定した時間だけ再生することができます。リラックス効果があるので睡眠用や作業用に使うことも可能です。
+
+    .box
+      .buttons
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 5}", @click="setMinutes(5)") 5分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 10}", @click="setMinutes(10)") 10分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 15}", @click="setMinutes(15)") 15分
+
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 20}", @click="setMinutes(20)") 20分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 25}", @click="setMinutes(25)") 25分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 30}", @click="setMinutes(30)") 30分
+
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 35}", @click="setMinutes(35)") 35分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 40}", @click="setMinutes(40)") 40分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 45}", @click="setMinutes(45)") 45分
+
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 50}", @click="setMinutes(50)") 50分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 55}", @click="setMinutes(55)") 55分
+        b-button(size="is-small", :disabled="isPlaying", :type="{'is-success': minute === 60}", @click="setMinutes(60)") 60分
+
+      .remaining-time {{ displayTime }}
+
+      div
+        b-button(icon-left="stop", type="is-danger", @click="stop", v-if="isPlaying") 終了
+        b-button(icon-left="play", type="is-success", outlined, @click="play", v-else) スタート
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPlaying: false,
+      audio: null,
+      audioContext: null,
+      remainingTime: 60 * 5,
+      timer: null,
+      minute: 5,
+      audios: [
+        'https://static.koeda.me/meditation/audios/a.mp3',
+        'https://static.koeda.me/meditation/audios/b.mp3',
+        'https://static.koeda.me/meditation/audios/c.mp3',
+        'https://static.koeda.me/meditation/audios/d.mp3',
+        'https://static.koeda.me/meditation/audios/e.mp3',
+      ],
+    }
+  },
+
+  computed: {
+    displayTime() {
+      const m = Math.floor(this.remainingTime / 60)
+
+      const mm = ('00' + m).slice(-2)
+      const s = this.remainingTime % 60
+      const ss = ('00' + s).slice(-2)
+
+      return `${mm}:${ss}`
+    },
+  },
+
+  mounted() {},
+
+  destroyed() {
+    this.stop()
+  },
+  methods: {
+    setAudio() {
+      const a = this.audios[Math.floor(Math.random() * this.audios.length)]
+      this.audio = new Audio(a)
+    },
+
+    play() {
+      this.isPlaying = true
+      this.setAudio()
+      this.audio.play()
+
+      this.timer = setInterval(this.countDown, 1000)
+    },
+
+    countDown() {
+      this.remainingTime = this.remainingTime - 1
+      if (this.remainingTime === 0) {
+        this.stop()
+      }
+    },
+
+    stop() {
+      this.isPlaying = false
+
+      clearInterval(this.timer)
+
+      if (this.audio) {
+        this.audio.pause()
+        this.audio = null
+      }
+
+      this.setMinutes(this.minute)
+    },
+
+    setMinutes(minute) {
+      this.minute = minute
+      this.remainingTime = 60 * minute
+    },
+  },
+}
+</script>
+
+<style scoped>
+.remaining-time {
+  font-size: 40px;
+  font-weight: bold;
+  font-family: monospace;
+}
+</style>
