05b46e028e219d8155d759c191b5c128e04d1792
💩 <69432552+unchidev@users.noreply.github.com>
2021-02-16 21:45:08 +0900

    フッター追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,16 @@
 <template lang="pug">
 div
   nuxt
+
+  footer.footer
+    .content.has-text-centered
+      p
+        | 提供：
+        a(href="https://koeda.me/", target="_blank" rel="noopener noreferrer") 肥溜め
+
+      p
+        | ソースコード：
+        a(href="https://github.com/koedame/meditation.koeda.me", target="_blank" rel="noopener noreferrer")
+          b-icon(icon="github")
+          | GitHub
 </template>
